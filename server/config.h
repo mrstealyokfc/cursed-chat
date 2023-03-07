05d4b124e44659007f3455f9133b5b11915484fe
cosway <admin@cosway.org>
2023-03-06 22:58:47 -0500

    some half-finished code

--- server/config.h
+++ server/config.h
@@ -1,3 +1,11 @@
 
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
 #define MESSAGE_LENGTH 4092
 #define PORT 1982
+#define MAX_CLIENTS 32
+#define SERVER_CLOSE_MESSAGE "Server is going DOWN!!!"
+
+
+#endif
--- server/main.c
+++ server/main.c
@@ -1,5 +1,8 @@
 #include<stdio.h>
 #include<unistd.h>
+#include<string.h>
+#include<pthread.h>
+#include<stdint.h>
 
 #include<sys/socket.h>
 #include<netinet/in.h>
@@ -8,60 +11,81 @@
 #include "server.h"
 #include "config.h"
 
-/*
-struct sockaddr_in address;
-int addrlen;
-
-int create_server(int port){
-	int server_fd;
-
-	//create socket descriptor
-	server_fd = socket(AF_INET,SOCK_STREAM,0);
-
-	//setting up the socket i guess.
-	int opt = 1;
-	int fail = setsockopt(server_fd, SOL_SOCKET,
-			   SO_REUSEADDR | SO_REUSEPORT,
-			   &opt, sizeof(opt));
-
-	if(fail<0)
-		printf("failed at setsockopt\n");
+int clients[MAX_CLIENTS];
 
 
-	addrlen = sizeof(address);
-
-	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = INADDR_ANY;
-	address.sin_port = htons(port);
-
-	fail = bind(server_fd, (struct sockaddr*)&address,
-		 sizeof(address));
-
-	if(fail<0)
-		printf("failed to bind\n");
-
-	fail = listen(server_fd,3);
-
-	if(fail< 0)
-		printf("failed to listen\n");
-
-	return server_fd;
+void add_new_client(int client_fd){
 
 }
-*/
+
+void* handle_client_connection_t (void* client_fd_ptr){
+	uint64_t index = (uint64_t)client_fd_ptr;
+
+	char message_buffer[MESSAGE_LENGTH+1];
+	memset(message_buffer,0,MESSAGE_LENGTH+1);
+	int read_len;
+	while(1){
+		read_len = read(clients[index], message_buffer, MESSAGE_LENGTH);
+		printf(message_buffer);
+		if(read_len == 0 || clients[index] == 0){
+			clients[index]=0;
+			close(clients[index]);
+			break;
+		}
+	}
+	return NULL;
+}
+
+void add_new_clients_t(server_s server){
+	while(1){
+		int new_client = await_client(server);
+		if(new_client < 0)
+			break;
+		add_new_client(new_client);
+	}
+}
+
+void* control_thread(void* args){
+
+	server_s server = *((server_s*)args);
+	char input[256];
+
+	while(1){
+		memset(input,0,256);
+
+		fgets(input,255,stdin);
+
+		if(strcmp(input,"stop\n") == 0){
+			close_server(server);
+			const char* cm = SERVER_CLOSE_MESSAGE;
+			for(int i=0;i<MAX_CLIENTS;i++){
+				send(clients[i],cm,strlen(cm),0);
+				close(clients[i]);
+			}
+			memset(clients,0,MAX_CLIENTS*4);
+			break;
+		}
+	}
+	return NULL;
+}
+
+void start_control_thread(server_s server){
+	pthread_t ptid;
+	pthread_create(&ptid,NULL,&control_thread,&server);
+}
+
 int main(){
 	printf("Program Started");
 	
 	server_s server = create_server(PORT);
 
+	start_control_thread(server);
+
 	int client_fd = await_client(server);
-	printf("%d\n",client_fd);
 
-	int client_2 = await_client(server);
-	printf("%d\n",client_2);
+	clients[0]=client_fd;
+	handle_client_connection_t(0);
 
-	close(client_2);
-	close(client_fd);
+	//close_server(server);
 
-	close_server(server);
 }
