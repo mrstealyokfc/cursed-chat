3c564dcfa834666418b57bf2a4d6bf91263282f5
= <dice.fridge@mail.com>
2023-03-07 22:02:09 -0500

    added names and commands without names and commands

---
+++ server/commands.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+#include "commands.h"
+#include "server.h"
+#include "config.h"
+
+
+void process_command(client_s* client, char* command){
+    printf("Command Detected!!!\n");
+}
---
+++ server/commands.h
@@ -0,0 +1,9 @@
+#ifndef __COMMANDS_H
+#define __COMMANDS_H
+
+#include "server.h"
+#include "config.h"
+
+void process_command(client_s* client,char* message);
+
+#endif // O
