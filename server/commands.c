962487ca72bf4d80708aa20215a00e957cf6b0de
= <dice.fridge@mail.com>
2023-03-07 22:07:57 -0500

    removed useless imports

--- server/commands.c
+++ server/commands.c
@@ -1,9 +1,6 @@
 #include<stdio.h>
 
 #include "commands.h"
-#include "server.h"
-#include "config.h"
-
 
 void process_command(client_s* client, char* command){
     printf("Command Detected!!!\n");
